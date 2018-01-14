83905c9f6c49fdbd5ae218909f82ee68285d6389
John Gray <johnny@johnnygray.co.uk>
2018-01-14 18:24:28 +0000

    Comments for sample.

--- Assets/Sample/Test.cs
+++ Assets/Sample/Test.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 
 public class Test : MonoBehaviour
 {
+	// This is an example of an AutoRef setting a reference to an
+	// instance of a class which exists on the same game object.
 	[AutoRef]
 	[SerializeField]
 	TestRef cTestRef;
 
+	// This is an example of an AutoRef being applied to an
+	// array of references to components on the same game object.
 	[AutoRef]
 	[SerializeField]
 	TestRef[] acTestRef;
 
+	// This is an example of an AutoRef being applied to an
+	// list of references to components on the same game object.
 	[AutoRef]
 	[SerializeField]
 	List<TestRef> lstTestRef;
 
+	// This is an example of an AutoRef being applied to an
+	// incompatible data type.
 	[AutoRef]
 	int nBadTestRef;
 }

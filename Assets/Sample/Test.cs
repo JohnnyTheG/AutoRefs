04b3943ede36fbc946efcad8347f1a02cdafac57
John Gray <johnny@johnnygray.co.uk>
2018-01-14 19:19:18 +0000

    More examples.

--- Assets/Sample/Test.cs
+++ Assets/Sample/Test.cs
@@ -7,22 +7,38 @@ public class Test : MonoBehaviour
 	// instance of a class which exists on the same game object.
 	[AutoRef]
 	[SerializeField]
-	TestRef cTestRef;
+	TestRef m_TestRef;
 
 	// This is an example of an AutoRef being applied to an
 	// array of references to components on the same game object.
 	[AutoRef]
 	[SerializeField]
-	TestRef[] acTestRef;
+	TestRef[] m_TestRefArray;
 
 	// This is an example of an AutoRef being applied to an
 	// list of references to components on the same game object.
 	[AutoRef]
 	[SerializeField]
-	List<TestRefAlternate> lstTestRefAlternative;
+	List<TestRefAlternate> m_TestRefAlternativeList;
 
 	// This is an example of an AutoRef being applied to an
 	// incompatible data type.
 	[AutoRef]
-	int nBadTestRef;
+	int m_BadTestRefInt;
+
+	// Example of AutoRef being used with a Unity built in component type.
+	[AutoRef]
+	[SerializeField]
+	Rigidbody m_Rigidbody;
+
+	// Example of AutoRef being used to get the transform of the GameObject.
+	// This is useful as it caches the Transform rather than .transform which gets it each time.
+	[AutoRef]
+	[SerializeField]
+	Transform m_Transform;
+
+	// Example of AutoRef being used to get all instances of a base class component.
+	[AutoRef]
+	[SerializeField]
+	List<Collider> m_Colliders;
 }
